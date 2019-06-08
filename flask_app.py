f5b4e07447260d38bfabda824c21d43f541898a1
coshmoney <coshm09@gmail.com>
2019-06-08 17:27:15 +0000

    Add SQLAlchemy and Comments DB table

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,31 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+# Configure and initialize the MySQL connection
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="coshmoney",
+    password="j4GG3dr0ck",
+    hostname="coshmoney.mysql.pythonanywhere-services.com",
+    databasename="coshmoney$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 comments = []
 
