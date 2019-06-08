e4a8bad4a651a696d4d99da3a9d5391bfde7675d
coshmoney <coshm09@gmail.com>
2019-06-08 16:28:17 +0000

    First commit of the Flask App

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+
+@app.route('/foo')
+def foo():
+    return 'This is the foo route!'
+
